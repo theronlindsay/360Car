29d1ae7da4e6975b9bf5b54ca890818dc4cf21a6
yasminadaniel <minadaniel72@gmail.com>
2024-12-13 10:49:40 -0700

    Update start button

---
+++ Assets/Scripts/ButtonVr.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class ButtonVr : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    AudioSource sound;
+    bool isPressed;
+    
+    void Start()
+    {
+        sound = GetComponent <AudioSource>();
+        isPressed = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, 0.003f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            sound.Play();
+            isPressed = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other == presser)
+        {
+            button.transform.localPosition = new Vector3(0, 0.015f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+}
